f46857267fe0dec626ae0504f3a9edae6585f334
Jeongmingz <joungmin0861@naver.com>
2023-03-06 17:47:07 +0900

    Fastapi

--- cemsController.py
+++ cemsController.py
@@ -4,6 +4,11 @@ from typing import List
 app = FastAPI()
 
 
+@app.get("/")
+async def api_home() -> dict:
+	return {'msg': 'CEMS Fast-API Main Page'}
+
+
 @app.get("/user")
 async def get_user_list() -> dict:
 	pass
@@ -11,7 +16,11 @@ async def get_user_list() -> dict:
 
 @app.get("/login")
 async def user_login() -> int:
-	pass
+	# login_check = 1 : 성공
+	# login_check = 0 : 실패 (실패 이유에 대해 설명해야 함. 비밀번호 오류, 존재하지 않는 계정 등)
+	login_check = 0
+
+	return login_check
 
 
 @app.put("/signup")
